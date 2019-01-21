7603daab104af1893b8c7877e1cff4e8ab5cfb6a
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-21 11:32:08 +0000

    extract environment variables to config.cfg

--- api/__init__.py
+++ api/__init__.py
@@ -7,20 +7,11 @@ from flask import Flask, request, Response
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # TODO - SECRET_KEY is used by Flask and extensions to keep data safe.
-        # It’s set to 'dev' to provide a convenient value during development, but it should be overridden with a random value when deploying.
-        # SECRET_KEY can be stored in instance/config.py 
-        SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI= os.environ['HOME'],
-        SQLALCHEMY_ECHO = True,
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    )
+    app.config.from_pyfile('config.cfg')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.cfg', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
