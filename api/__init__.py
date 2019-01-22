ba1863864fa9ed559c41ec8aa1142f0a6f0ca1c0
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-22 11:02:40 +0000

    "flask run" creates database tables

--- api/__init__.py
+++ api/__init__.py
@@ -2,19 +2,18 @@ import os
 import json
 
 from api.messages_controller import conversations_api
-from flask import Flask, request, Response, Blueprint
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
     app.register_blueprint(conversations_api)
 
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.cfg', silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -25,11 +24,15 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # create db instance
+    global db
+    db = SQLAlchemy(app)
+    from api import models
+    db.create_all()
+
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-    
-    
 
     return app
--- api/models.py
+++ api/models.py
@@ -1,20 +1,9 @@
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
-
-app = Flask(__name__)
-db = SQLAlchemy(app)
+from api import db
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, nullable=False)
+    receiver_id = db.Column(db.Integer, nullable=False)
     content = db.Column(db.String(140), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-class Conversation(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    
-
-class ConversationUserJoin(db.Model):
-    conversation_id = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
