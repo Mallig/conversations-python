a4d1f02edf3bbfb4cdf8c4ae8bcc3428ad587aa1
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-22 11:06:41 +0000

    set table name to plural

--- api/models.py
+++ api/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from api import db
 
 class Message(db.Model):
+    __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
     sender_id = db.Column(db.Integer, nullable=False)
     receiver_id = db.Column(db.Integer, nullable=False)
