b9b9ca801807fa6c1ec7c3553f9900317d57474b
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-28 14:48:29 +0000

    move database setup fixture to conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,8 +1,42 @@
 import pytest
-from api import create_app
+from api import create_app, db
 
 @pytest.fixture
 def app():
     app = create_app('test_config.py')
     app.debug = True
     return app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+@pytest.fixture
+def setup_database():
+    print('in the database setup fixture')
+    clear_database_tables(db)
+    seed_database(db)
+
+
+def clear_database_tables(db):
+    db.drop_all()
+    db.create_all()
+
+def seed_database(db):
+    from api.models import Message, Conversation, ConversationUserJoin
+
+    convo = Conversation()
+    db.session.add(convo)
+    db.session.commit()
+
+    db.session.add(ConversationUserJoin(conversation_id = convo.id, user_id = 1))
+    db.session.add(ConversationUserJoin(conversation_id = convo.id, user_id = 2))
+    db.session.commit()
+
+    from data import conversation
+    for message in conversation:
+        db.session.add(Message(
+            sender_id = message['sender_id'],
+            conversation_id = convo.id,
+            content = message['content']
+        ))
--- test/integration/test_get_conversation.py
+++ test/integration/test_get_conversation.py
@@ -1,37 +1,6 @@
 import pytest
 from flask import url_for
 
-def clear_database_tables(db):
-    db.drop_all()
-    db.create_all()
-
-def seed_database(db):
-    from api.models import Message, Conversation, ConversationUserJoin
-
-    convo = Conversation()
-    db.session.add(convo)
-    db.session.commit()
-
-    db.session.add(ConversationUserJoin(conversation_id = convo.id, user_id = 1))
-    db.session.add(ConversationUserJoin(conversation_id = convo.id, user_id = 2))
-    db.session.commit()
-
-    from data import conversation
-    for message in conversation:
-        db.session.add(Message(
-            sender_id = message['sender_id'],
-            conversation_id = convo.id,
-            content = message['content']
-        ))
-
-@pytest.fixture
-def setup_database():
-    print('in the database setup fixture')
-    from api import db
-    clear_database_tables(db)
-    seed_database(db)
-
-
 class TestApp:
     def test_get_convo_by_id(self, client, setup_database):
         res = client.get(url_for('conversation_api.get_single_conversation_by_id', conversation_id=1))
