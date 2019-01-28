2ffbed32ec733d6979fd0918cd28d40faa251704
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-01-28 12:27:02 +0000

    move tests to unit test directory

--- test/unit/test_conversation_controller.py
+++ test/unit/test_conversation_controller.py
@@ -1,8 +1,17 @@
 import pytest
 from flask import url_for
 
+@pytest.fixture
+def setup_database():
+    print('in the database setup fixture')
+    from api import db
+    db.drop_all()
+    db.create_all()
+
+
 class TestApp:
-    def test_get_convo_by_id(self, client):
+    def test_get_convo_by_id(self, client, setup_database):
+        setup_database()
         res = client.get(url_for('conversation_api.get_single_conversation_by_id', conversation_id=1))
         assert res.status_code == 200
         assert res.json == [{
