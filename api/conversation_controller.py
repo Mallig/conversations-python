15d292e9e44ca152f2f1d69d0d90f98f47dd4a3a
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-02-13 21:25:26 +0000

    change conversation service name

--- api/conversation_controller.py
+++ api/conversation_controller.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, Response, Blueprint, jsonify
-from api import ConversationService
+from api import conversation_service
 
 conversation_api = Blueprint('conversation_api', __name__)
 
 @conversation_api.route("/conversation/<int:conversation_id>/id", methods=["GET"])
 def get_single_conversation_by_id(conversation_id):
-    response = ConversationService.conversation_messages(conversation_id)
+    response = conversation_service.conversation_messages(conversation_id)
     return jsonify(response)
 
 @conversation_api.route("/messages", methods=['POST'])
@@ -14,11 +14,11 @@ def post_messages():
     user_ids = [json_data['sender_id']]
     user_ids.extend(json_data['receiver_ids'])
 
-    conversation_id = ConversationService.find_or_create_conversation(user_ids)
-    response = ConversationService.create_and_commit_message(json_data, conversation_id)
+    conversation_id = conversation_service.find_or_create_conversation(user_ids)
+    response = conversation_service.create_and_commit_message(json_data, conversation_id)
     return jsonify(response)
 
 @conversation_api.route("/conversation/<int:user_id>/latest", methods=['GET'])
 def get_conversation(user_id):
-    response = ConversationService.latest_conversations(user_id)
+    response = conversation_service.latest_conversations(user_id)
     return jsonify(response)
