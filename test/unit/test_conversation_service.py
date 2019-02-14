0976947bce0de5d6b58a5561e5455e3df21b0641
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-02-14 16:29:15 +0000

    test creating invalid message

--- test/unit/test_conversation_service.py
+++ test/unit/test_conversation_service.py
@@ -17,11 +17,14 @@ class TestConversationService:
         messages = conversation_service.conversation_messages(1, db_session)
         assert messages == conversation_request
 
-    def test_create_and_commit_message(self, client, setup_database):
+    def test_create_and_commit_message(self, client, setup_database, db_session):
         valid_json_message = { "content": "test message", "sender_id": 1, "receiver_ids": [2] }
-        commit_response = conversation_service.create_and_commit_message(valid_json_message)
+        commit_response = conversation_service.create_and_commit_message(valid_json_message, db_session)
         assert commit_response == { "saved": True }
 
-    # def test_create_and_commit_invalid_message(self, client, setup_database):
+    def test_create_and_commit_invalid_message(self, client, setup_database, db_session):
+        invalid_json_message = { "content": None, "sender_id": 1, "receiver_ids": [2] }
+        commit_response = conversation_service.create_and_commit_message(invalid_json_message, db_session)
+        assert commit_response == { "saved": False, 'error': 'null value in column "content" violates not-null constraint' }
 
     # def test_get_latest_conversations():
